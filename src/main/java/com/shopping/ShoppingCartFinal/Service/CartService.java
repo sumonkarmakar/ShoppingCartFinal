a8fbc873a94f188d7e1d75f7d91ff3a5c1741e02
Sumon Karmakar <suman.talkto@gmail.com>
2018-02-21 07:00:43 +0530

    Add files via upload

--- src/main/java/com/shopping/ShoppingCartFinal/Service/CartService.java
+++ src/main/java/com/shopping/ShoppingCartFinal/Service/CartService.java
@@ -1,5 +1,19 @@
 package com.shopping.ShoppingCartFinal.Service;
 
-public interface CartService {
+import java.util.List;
 
+import com.shopping.ShoppingCartFinal.Model.Cart;
+
+public interface CartService {
+	public void add(Cart Cart);
+
+	public Cart getById(int id);
+
+	public void update(Cart Cart);
+
+//	public Cart delete(int id);
+
+	public List<Cart> getAll();
+
+	public void delete(Cart Cart);
 }
